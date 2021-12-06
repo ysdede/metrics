683e6b76b814fcbc9404637ddfc17b5da89bf114
Yunus <yunusseyhandede@gmail.com>
2021-12-06 16:46:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
       //insertStylesheet('http://amio.github.io/gh-index/index.css')
       insertStylesheet('index.css')
-      insertStylesheetAsync('https://octicons.github.com/components/octicons/octicons/octicons.css')
+      insertStylesheetAsync('https://cdnjs.cloudflare.com/ajax/libs/octicons/3.5.0/octicons.min.css');
       window.fetch || insertScript('https://cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js')
 
       index.loadTrees()
